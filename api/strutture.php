38abc8932de78e858e345bd8e4903a066ddb07c6
Mariasilvia <mariasilviavanni@yahoo.it>
2018-05-18 11:49:07 +0200

    aggiunta di strutture.php

---
+++ api/strutture.php
@@ -0,0 +1,26 @@
+<?php
+include('config.php');
+header('Content-Type: application/json');
+
+$sql = "SELECT * FROM strutture";
+
+$risultato = mysqli_query($conn,$sql);
+if (!$risultato) die("Errore query: ".mysqli_error($conn));
+
+$return = array();
+
+while($riga=mysqli_fetch_assoc($risultato))
+{
+	$riga['Nome'] = ucfirst(strtolower($riga['Nome']));
+	$riga['Numero'] = intval($riga['Numero']);
+	$riga['Latitudine'] = floatval($riga['Latitudine']);
+	$riga['Longitudine'] = floatval($riga['Longitudine']);
+		
+	$return[] = $riga;
+}
+
+echo json_encode($return);
+mysqli_close($conn);
+
+
+?>	
