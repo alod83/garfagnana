d279c4e8f6b304b343e8b682f8ae1c4ad15f1a73
Angelica Lo Duca <angelica.loduca@iit.cnr.it>
2018-06-01 16:09:25 +0200

    corretti errori

--- js/prova.js
+++ js/prova.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
 
 $("#menutendina").on("change",function(){
 var tipo=$("#cerca").val();
- $.getJSON('api/prova.php', {tipo:tipo}, function (data){
+console.log(tipo);
+ $.getJSON('api/prova.php?Tipologia=' + tipo, function (data){
  console.log(data);
 
-
+		$("#contenitore").empty();
 		for(var i = 0; i < data.length; i++)
 		{	$("#contenitore").append('<h2>' + data[i].Nome + '</h2>');
 			$("#contenitore").append('Descrizione:'+'<p id="descrizione">' + data[i].Descrizione + '</p>');
